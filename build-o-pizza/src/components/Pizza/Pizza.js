c49b7128a048d92af16488cd5abdc16df22e550f
Olena Drugalya <helendrugalya@gmail.com>
2021-01-11 15:49:36 +0100

    added styled components to Pepperoni

--- build-o-pizza/src/components/Pizza/Pizza.js
+++ build-o-pizza/src/components/Pizza/Pizza.js
@@ -1,28 +1,29 @@
 import PizzaIngredient from "./PizzaIngredient/PizzaIngredient";
 import styled from "styled-components";
 
+const StyledPizza = styled.div `
+    width: 100%;
+    height: 100%;
+    margin: auto;
+    text-align: center;
+    font-weight: bold;
+    font-size: 1.2rem;
+    @media (min-width: 1000px) and (min-height: 700px) {
+      width: 700px;
+      height: 600px;
+    }
+    @media (min-width: 500px) and (min-height: 400px) {
+      width: 450px;
+      height: 400px;
+    }
+  `
+
 const Pizza = ({ ingredients }) => {
   let ingredientsArray = Object.keys(ingredients).map((item) => {
     return [ingredients[item]].map((elem, index) => {
       return <PizzaIngredient key={item + index} type={item} quantity={elem} />;
     });
   });
-
-  const StyledPizza = styled.div`
-    width: 100%;
-  height: 100%;
-  margin: auto;
-  text-align: center;
-  font-weight: bold;
-  font-size: 1.2rem;
-}
-@media (min-width: 1000px) and (min-height: 700px) {
-    width: 700px;
-    height: 600px;
-}
-@media (min-width: 500px) and (min-height: 400px) {
-    width: 450px;
-    height: 400px;`;
 
   return (
     <StyledPizza>
--- build-o-pizza/src/components/Pizza/PizzaIngredient/Pepperoni/Pepperoni.js
+++ build-o-pizza/src/components/Pizza/PizzaIngredient/Pepperoni/Pepperoni.js
@@ -1,10 +1,22 @@
-import classes from "./Pepperoni.module.css";
+import styled from "styled-components";
 
 const Pepperoni = ({ pepMargin, children }) => {
-  return (
-    <div className={classes.Pepperoni} style={{ margin: `${pepMargin}` }}>
-      {children}
-    </div>
-  );
+  const StyledPepperoni = styled.div`
+    position: absolute;
+    width: 80px;
+    height: 70px;
+    border-radius: 60px;
+    background: hsl(26, 96%, 18%);
+    z-index: 12;
+    font-size: 2em;
+    text-align: center;
+    padding-top: 10px;
+    box-shadow: inset 0 0 85px #711712;
+    border: 2px solid #871b15;
+    text-indent: -9999px;
+    color: #fff;
+    margin: ${pepMargin};
+  `;
+  return <StyledPepperoni>{children}</StyledPepperoni>;
 };
 export default Pepperoni;
