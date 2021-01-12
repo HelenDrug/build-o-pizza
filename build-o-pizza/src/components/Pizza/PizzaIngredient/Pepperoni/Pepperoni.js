cb63885b2d6f7bf15a4b21f8a9d093b3eccbaf6e
Olena Drugalya <helendrugalya@gmail.com>
2021-01-12 09:17:00 +0100

    added new file with styled pepperoni - SimplePepperoni.js

--- build-o-pizza/src/components/Pizza/PizzaIngredient/Pepperoni/Pepperoni.js
+++ build-o-pizza/src/components/Pizza/PizzaIngredient/Pepperoni/Pepperoni.js
@@ -1,22 +1,7 @@
-import styled from "styled-components";
+import SimplePepperoni from "./SimplePepperoni";
 
 const Pepperoni = ({ pepMargin, children }) => {
-  const StyledPepperoni = styled.div`
-    position: absolute;
-    width: 80px;
-    height: 70px;
-    border-radius: 60px;
-    background: hsl(26, 96%, 18%);
-    z-index: 12;
-    font-size: 2em;
-    text-align: center;
-    padding-top: 10px;
-    box-shadow: inset 0 0 85px #711712;
-    border: 2px solid #871b15;
-    text-indent: -9999px;
-    color: #fff;
-    margin: ${pepMargin};
-  `;
-  return <StyledPepperoni>{children}</StyledPepperoni>;
+  return <SimplePepperoni pepMargin={pepMargin}>{children}</SimplePepperoni>;
 };
+
 export default Pepperoni;
---
+++ build-o-pizza/src/components/Pizza/PizzaIngredient/Pepperoni/SimplePepperoni.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const SimplePepperoni = styled.div`
+  position: absolute;
+  width: 80px;
+  height: 70px;
+  border-radius: 60px;
+  background: hsl(26, 96%, 18%);
+  z-index: 12;
+  font-size: 2em;
+  text-align: center;
+  padding-top: 10px;
+  box-shadow: inset 0 0 85px #711712;
+  border: 2px solid #871b15;
+  text-indent: -9999px;
+  color: #fff;
+  margin: ${(props) => props.pepMargin};
+`;
+
+export default SimplePepperoni;
