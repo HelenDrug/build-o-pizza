d9e54efec9e999fbdd916f0a70811a564a8bafdc
HelenDrug <helendrugalya@gmail.com>
2021-02-05 10:05:56 +0100

    added ToolBar component layout

---
+++ build-o-pizza/src/components/Navigation/ToolBar/ToolBar.js
@@ -0,0 +1,13 @@
+import { Header, Menu, Logo, MainNav, NavList } from "./ToolBar.styles";
+
+export const ToolBar = () => {
+  return (
+    <Header>
+      <Menu>Menu</Menu>
+      <Logo>Logo</Logo>
+      <MainNav>
+        <NavList>...</NavList>
+      </MainNav>
+    </Header>
+  );
+};
---
+++ build-o-pizza/src/components/Navigation/ToolBar/ToolBar.styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Header = styled.header``;
+
+export const Menu = styled.div``;
+
+export const Logo = styled.div``;
+
+export const MainNav = styled.nav``;
+
+export const NavList = styled.ul``;
